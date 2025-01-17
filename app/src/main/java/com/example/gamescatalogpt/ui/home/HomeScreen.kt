f707f71e469aaa9f54a0470e34a4df19fc1f12f1
Jhohells Erick Dianderas Lopez <jhohellserick@gmail.com>
2025-01-17 06:34:28 -0500

    update home screen

--- app/src/main/java/com/example/gamescatalogpt/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/gamescatalogpt/ui/home/HomeScreen.kt
@@ -1,18 +1,38 @@
 package com.example.gamescatalogpt.ui.home
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.navigation.NavController
+import com.example.gamescatalogpt.domain.models.Game
+import com.example.gamescatalogpt.navigation.AppNavigator
+import com.example.gamescatalogpt.ui.components.GameItem
+import org.koin.androidx.compose.koinViewModel
+import org.koin.compose.koinInject
 
 @Composable
-fun HomeScreen() {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        Text("Home Screen")
+fun HomeScreen(
+    navController: NavController,
+    homeViewModel: HomeViewModel = koinViewModel(),
+    navigator: AppNavigator = koinInject()
+){
+    val games: List<Game> by homeViewModel.games.collectAsState()
+    val favorites by homeViewModel.favorites.collectAsState()
+
+    LazyColumn {
+        items(games.size) { index ->
+            val game = games[index]
+            GameItem(
+                game = game,
+                isFavorite = game.id in favorites,
+                onFavoriteToggle = {
+                    homeViewModel.toggleFavorite(game.id)
+                },
+                onClickItem = {
+                    navigator.navigateToDetail(navController, game.id, true)
+                }
+            )
+        }
     }
 }
